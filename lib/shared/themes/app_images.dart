0c28fe70146ffede70a4f1fea143fa6584ff474a
Airon Gabriel <ayrongabriel@gmail.com>
2021-06-23 17:41:58 -0300

    Update image pesona com icones

--- lib/shared/themes/app_images.dart
+++ lib/shared/themes/app_images.dart
@@ -3,5 +3,6 @@ class AppImages {
   static const logomini = "assets/images/logomini.png";
   static const union = "assets/images/union.png";
   static const person = "assets/images/person.png";
+  static const personIcon = "assets/images/person-icon.png";
   static const google = "assets/images/google.png";
 }
