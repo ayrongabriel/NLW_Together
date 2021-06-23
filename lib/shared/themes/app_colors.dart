56bddd94a884fa2ed7a53fd2e2b8062c47807e58
Airon Gabriel <ayrongabriel@gmail.com>
2021-06-23 17:42:47 -0300

    Color gradientes

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -12,12 +12,20 @@ class AppColors {
   static final linerGradientLogin = LinearGradient(
     colors: [
       Color.fromRGBO(255, 255, 255, 0),
-      Color.fromRGBO(255, 255, 255, 0.8),
-      Color.fromRGBO(255, 255, 255, 1),
+      Color.fromRGBO(255, 255, 255, 0.7),
+      Color.fromRGBO(255, 255, 255, 0.9),
       Color.fromRGBO(255, 255, 255, 1),
       Color.fromRGBO(255, 255, 255, 1),
     ],
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter,
+  );
+  static final radialGradientLogin = RadialGradient(
+    colors: [
+      Color(0xFFFFC380),
+      Color(0xFFFF941A),
+    ],
+    center: Alignment(0, 0.5),
+    radius: 0.8,
   );
 }
